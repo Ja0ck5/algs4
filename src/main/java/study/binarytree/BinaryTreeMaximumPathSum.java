5a0772fe6ffce55614ac303880d027abb08df72f
liyanjie2 <liyanjie2@yy.com>
2021-02-22 21:19:05 +0800

    BinaryTreeMaximumPathSum

---
+++ src/main/java/study/binarytree/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,38 @@
+package study.binarytree;
+
+public class BinaryTreeMaximumPathSum {
+
+    private class ResultType {
+        int singlePath, maxPath;
+
+        public ResultType(int singlePath, int maxPath) {
+            this.singlePath = singlePath;
+            this.maxPath = maxPath;
+        }
+    }
+
+    private ResultType helper(TreeNode root) {
+        if (root == null) {
+            return new ResultType(0, Integer.MIN_VALUE);
+        }
+        // Divide
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+
+        // Conquer
+        int singlePath = Math.max(left.singlePath, right.singlePath) + root.val;
+        singlePath = Math.max(0, singlePath);
+
+        int maxPath = Math.max(left.maxPath, right.maxPath);
+        maxPath = Math.max(maxPath, left.singlePath + right.singlePath + root.val);
+
+        return new ResultType(singlePath, maxPath);
+
+    }
+
+    public int maxPathSum(TreeNode root) {
+        ResultType result = helper(root);
+        return result.maxPath;
+    }
+
+}
