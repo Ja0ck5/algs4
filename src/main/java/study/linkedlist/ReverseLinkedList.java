6a1cc349d0ef9284823e8ce97f46a3b46a26d2b8
liyanjie2 <liyanjie2@yy.com>
2021-02-28 11:51:33 +0800

    ReverseLinkedList

---
+++ src/main/java/study/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,16 @@
+package study.linkedlist;
+
+public class ReverseLinkedList {
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = prev;
+            prev = head;
+            head = tmp;
+        }
+        return prev;
+    }
+
+}
