7d149e46ffe7f271682d89c019a691601c91b24c
liyanjie2 <liyanjie2@yy.com>
2021-02-28 15:20:52 +0800

    SortList merge & fast slow point

--- src/main/java/study/linkedlist/LinkedListOutLine.java
+++ src/main/java/study/linkedlist/LinkedListOutLine.java
@@ -3,7 +3,13 @@ package study.linkedlist;
 /**
  * 1. DummyNode 好处
  * Scenario : When the head is not determinated.
- * 2. Basic skills in linkedlist u should know
+ * 2. Basic skills in linked list u should know
+ * - Insert a node in sorted list
+ * - Remove a node from linked list
+ * - Reverse a linked list
+ * - Merge two linked list
+ * - Find a middle of a linked list
+ *
  * 3. Fast Slow Point
  */
 public class LinkedListOutLine {
---
+++ src/main/java/study/linkedlist/SortList.java
@@ -0,0 +1,53 @@
+package study.linkedlist;
+
+/**
+ * *****
+ * Merge & fast slow point
+ */
+public class SortList {
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = findMiddle(head);
+
+        ListNode right = sortList(mid.next);
+        mid.next = null;
+        ListNode left = sortList(head);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                tail.next = left;
+                left = left.next;
+            } else {
+                tail.next = right;
+                right = right.next;
+            }
+            tail = tail.next;
+        }
+        if (left != null) {
+            tail.next = left;
+        }
+        if (right != null) {
+            tail.next = right;
+        }
+        return dummy.next;
+    }
+
+    private ListNode findMiddle(ListNode head) {
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+}
