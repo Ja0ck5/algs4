00bc4326b92dc09f4979b744e386089589674106
liyanjie2 <liyanjie2@yy.com>
2021-03-03 20:00:31 +0800

    DP outline

--- src/main/java/study/dynamic/DynamicProgramOutLine.java
+++ src/main/java/study/dynamic/DynamicProgramOutLine.java
@@ -2,12 +2,24 @@ package study.dynamic;
 
 /**
  * - 从递归到动态规划
- *
- *
- *
  * - 矩阵上的动态规划
  * - 序列上的动态规划
  * - 动态规划的分类
+ * 1. Matrix DP
+ * 2. Sequence
+ * 3. Two Sequence DP
+ * 4. BackPack
+ *
+ * - 如何使用动态规划
+ * 1. 求最大最小
+ * 2. 求 true / false
+ * 3. 求 解的个数
+ *
+ * 动态规划4要素
+ * 1. 状态(存储小规模问题的结果)
+ * 2. 方程 Function(如何通过小的结果求大的结果)
+ * 3. 初始化，最极限的最小状态是什么
+ * 4. 答案，最大的结果是什么
  */
 public class DynamicProgramOutLine {
 }
