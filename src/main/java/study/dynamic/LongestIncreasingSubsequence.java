816863e2982b5340f8aff3a71b616ef6d6e4b5e9
liyanjie2 <liyanjie2@yy.com>
2021-03-04 23:19:12 +0800

    LongestIncreasingSubsequence DP

---
+++ src/main/java/study/dynamic/LongestIncreasingSubsequence.java
@@ -0,0 +1,122 @@
+package study.dynamic;
+
+/**
+ * 300. 最长递增子序列
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ * <p>
+ * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [10,9,2,5,3,7,101,18]
+ * 输出：4
+ * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,1,0,3,2,3]
+ * 输出：4
+ * 示例 3：
+ * <p>
+ * 输入：nums = [7,7,7,7,7,7,7]
+ * 输出：1
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 2500
+ * -104 <= nums[i] <= 104
+ * <p>
+ * <p>
+ * 进阶：
+ * <p>
+ * 你可以设计时间复杂度为 O(n2) 的解决方案吗？
+ * 你能将算法的时间复杂度降低到 O(n log(n)) 吗?
+ */
+public class LongestIncreasingSubsequence {
+
+    /**
+     * O
+     * (
+     * n
+     * 2
+     * )
+     * 解法：
+     * <p>
+     * Dpi 表示以第i个数字为结尾的最长上升子序列的长度。 对于每个数字，枚举前面所有小于自己的数字 j，Dpi = max{Dpj} + 1. 如果没有比自己小的，Dpi = 1;
+     *
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = dp[i] < dp[j] + 1 ? dp[j] + 1 : dp[i];
+                }
+            }
+            if (max < dp[i]) {
+                max = dp[i];
+            }
+        }
+        return max;
+    }
+
+
+    /**
+     * O
+     * (
+     * n
+     * l
+     * o
+     * g
+     * n
+     * )
+     * 解法：
+     * <p>
+     * 使用一个辅助空间B数组。 Bi存储Dp值为i的最小的数字。（有多个位置，以这些位置为结尾的LIS长度都为i， 则这些数字中最小的一个存在Bi中） 则B数组严格递增。且下标表示LIS长度，也是严格递增，可以在B数组中进行二分查找。
+     * <p>
+     * 对于每个位置i，我们要找，所有小于Ai, 且Dp值最大的那个。这个操作在B数组中二分查找。
+     */
+    public int longestIncreasingSubsequence(int[] nums) {
+        int[] minLast = new int[nums.length + 1];
+        minLast[0] = Integer.MIN_VALUE;
+        for (int i = 1; i <= nums.length; i++) {
+            minLast[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            // find the first number in minLast >= nums[i]
+            int index = binarySearch(minLast, nums[i]);
+            minLast[index] = nums[i];
+        }
+
+        for (int i = nums.length; i >= 1; i--) {
+            if (minLast[i] != Integer.MAX_VALUE) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    // find the first number > num
+    private int binarySearch(int[] minLast, int num) {
+        int start = 0, end = minLast.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (minLast[mid] < num) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        return end;
+    }
+
+
+}
