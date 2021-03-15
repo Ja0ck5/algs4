e4f4fa9fae806af86c091d737d1f94b22546dbe8
liyanjie2 <liyanjie2@yy.com>
2021-03-15 20:30:47 +0800

    MaximumWidthOfBinaryTree

---
+++ src/main/java/study/binarytree/MaximumWidthOfBinaryTree.java
@@ -0,0 +1,115 @@
+package study.binarytree;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * 662. 二叉树最大宽度
+ * 给定一个二叉树，编写一个函数来获取这个树的最大宽度。树的宽度是所有层中的最大宽度。这个二叉树与满二叉树（full binary tree）结构相同，但一些节点为空。
+ *
+ * 每一层的宽度被定义为两个端点（该层最左和最右的非空节点，两端点间的null节点也计入长度）之间的长度。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ *
+ *            1
+ *          /   \
+ *         3     2
+ *        / \     \
+ *       5   3     9
+ *
+ * 输出: 4
+ * 解释: 最大值出现在树的第 3 层，宽度为 4 (5,3,null,9)。
+ * 示例 2:
+ *
+ * 输入:
+ *
+ *           1
+ *          /
+ *         3
+ *        / \
+ *       5   3
+ *
+ * 输出: 2
+ * 解释: 最大值出现在树的第 3 层，宽度为 2 (5,3)。
+ * 示例 3:
+ *
+ * 输入:
+ *
+ *           1
+ *          / \
+ *         3   2
+ *        /
+ *       5
+ *
+ * 输出: 2
+ * 解释: 最大值出现在树的第 2 层，宽度为 2 (3,2)。
+ * 示例 4:
+ *
+ * 输入:
+ *
+ *           1
+ *          / \
+ *         3   2
+ *        /     \
+ *       5       9
+ *      /         \
+ *     6           7
+ * 输出: 8
+ * 解释: 最大值出现在树的第 4 层，宽度为 8 (6,null,null,null,null,null,null,7)。
+ * 注意: 答案在32位有符号整数的表示范围内。
+ */
+public class MaximumWidthOfBinaryTree {
+
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        int max = 0;
+        root.val = 0;
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            max = Math.max(queue.getLast().val - queue.getFirst().val + 1, max);
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if(node.left != null){
+                    node.left.val = node.val * 2 + 1;
+                    queue.offer(node.left);
+                }
+                if(node.right != null){
+                    node.right.val = node.val * 2 + 2;
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return max;
+    }
+
+
+    int ans;
+    Map<Integer, Integer> left;
+
+    public int widthOfBinaryTreeV2(TreeNode root) {
+        ans = 0;
+        left = new HashMap();
+        dfs(root, 0, 0);
+        return ans;
+    }
+
+    public void dfs(TreeNode root, int depth, int pos) {
+        if (root == null) return;
+        left.putIfAbsent(depth, pos);
+        ans = Math.max(ans, pos - left.get(depth) + 1);
+        dfs(root.left, depth + 1, 2 * pos);
+        dfs(root.right, depth + 1, 2 * pos + 1);
+    }
+
+
+}
