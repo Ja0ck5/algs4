ec3957d8ff9661072e8889d32948910a5ed12a5c
liyanjie2 <liyanjie2@yy.com>
2021-03-18 17:55:49 +0800

    lowestCommonAncestorVersion3

--- src/main/java/study/binarytree/LowestCommonAncestor.java
+++ src/main/java/study/binarytree/LowestCommonAncestor.java
@@ -1,38 +1,36 @@
 package study.binarytree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
- *
  * 235. 二叉搜索树的最近公共祖先
  * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
- *
+ * <p>
  * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
- *
+ * <p>
  * 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
- *
- *
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
  * 输出: 6
  * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
  * 示例 2:
- *
+ * <p>
  * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
  * 输出: 2
  * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
- *
- *
+ * <p>
+ * <p>
  * 说明:
- *
+ * <p>
  * 所有节点的值都是唯一的。
  * p、q 为不同节点且均存在于给定的二叉搜索树中。
- *
+ * <p>
  * 最近公共祖先
  */
 public class LowestCommonAncestor {
@@ -74,6 +72,7 @@ public class LowestCommonAncestor {
 
     /**
      * 二叉树(非搜索树)
+     *
      * @param root
      * @param node1
      * @param node2
@@ -109,6 +108,7 @@ public class LowestCommonAncestor {
 
     /**
      * 二叉搜索树
+     *
      * @param root
      * @param p
      * @param q
@@ -123,5 +123,37 @@ public class LowestCommonAncestor {
         return root;
     }
 
+
+    public TreeNode lowestCommonAncestorVersion3(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        Map<TreeNode, TreeNode> map = new HashMap<>();
+        map.put(root, null);
+        fillParentMap(root, map);
+        Set<TreeNode> set = new HashSet<>();
+        TreeNode tmp = p;
+        set.add(tmp);
+        while (map.get(tmp) != null) {
+            tmp = map.get(tmp);
+            set.add(tmp);
+        }
+        tmp = q;
+        while (!set.contains(tmp)) {
+            tmp = map.get(tmp);
+        }
+        return tmp;
+    }
+
+    public void fillParentMap(TreeNode root, Map<TreeNode, TreeNode> map) {
+        if (root.left != null) {
+            map.put(root.left, root);
+            fillParentMap(root.left, map);
+        }
+        if (root.right != null) {
+            map.put(root.right, root);
+            fillParentMap(root.right, map);
+        }
+    }
 
 }
