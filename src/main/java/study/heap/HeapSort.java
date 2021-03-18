658a7169eb7a3ec60cc0e50a25f4124235716d05
liyanjie2 <liyanjie2@yy.com>
2021-03-18 21:59:33 +0800

    HeapSort

---
+++ src/main/java/study/heap/HeapSort.java
@@ -0,0 +1,36 @@
+package study.heap;
+
+public class HeapSort {
+
+    public void heapSort(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            heapify(arr, i, arr.length);
+        }
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+        while (heapSize > 0) {
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
+    }
+
+    private void heapify(int[] arr, int index, int heapSize) {
+        int left = index * 2 + 1;
+        while (left < heapSize) {
+            int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+            largest = arr[largest] > arr[index] ? largest : index;
+            if (largest == index) {
+                break;
+            }
+            index = largest;
+            left = left * 2 + 1;
+        }
+    }
+
+    public void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+
+}
