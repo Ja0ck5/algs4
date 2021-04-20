67a95f7b355abab49c39c4ab1fae99e8e4618a8d
liyanjie2 <liyanjie2@yy.com>
2021-04-20 10:38:44 +0800

    MaxmumSubarray

---
+++ src/main/java/study/bd/MaximumSubarray.java
@@ -0,0 +1,53 @@
+package study.bd;
+
+/**
+ * 53. 最大子序和
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ * 示例 2：
+ *
+ * 输入：nums = [1]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：nums = [0]
+ * 输出：0
+ * 示例 4：
+ *
+ * 输入：nums = [-1]
+ * 输出：-1
+ * 示例 5：
+ *
+ * 输入：nums = [-100000]
+ * 输出：-100000
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -105 <= nums[i] <= 105
+ * @author liyanjie
+ * @createTime 2021-04-20 10:33
+ */
+public class MaximumSubarray {
+
+    public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int pre = 0 , max = nums[0];
+        for (int num : nums) {
+            pre = Math.max(pre + num, num);
+            max = Math.max(pre, max);
+        }
+        return max;
+    }
+
+}
