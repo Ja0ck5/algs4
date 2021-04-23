0689be9b445501c6cf3ed15f76fae8a5f38acf48
liyanjie2 <liyanjie2@yy.com>
2021-04-23 09:45:16 +0800

    RDE

---
+++ src/main/java/study/bd/RussianDollEnvelopes.java
@@ -0,0 +1,84 @@
+package study.bd;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 354. 俄罗斯套娃信封问题
+ * 给你一个二维整数数组 envelopes ，其中 envelopes[i] = [wi, hi] ，表示第 i 个信封的宽度和高度。
+ *
+ * 当另一个信封的宽度和高度都比这个信封大的时候，这个信封就可以放进另一个信封里，如同俄罗斯套娃一样。
+ *
+ * 请计算 最多能有多少个 信封能组成一组“俄罗斯套娃”信封（即可以把一个信封放到另一个信封里面）。
+ *
+ * 注意：不允许旋转信封。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：envelopes = [[5,4],[6,4],[6,7],[2,3]]
+ * 输出：3
+ * 解释：最多信封的个数为 3, 组合为: [2,3] => [5,4] => [6,7]。
+ * 示例 2：
+ *
+ * 输入：envelopes = [[1,1],[1,1],[1,1]]
+ * 输出：1
+ *
+ *
+ * 提示：
+ *
+ * 1 <= envelopes.length <= 5000
+ * envelopes[i].length == 2
+ * 1 <= wi, hi <= 104
+ *
+ *
+ * @author liyanjie
+ * @createTime 2021-04-21 12:45
+ */
+public class RussianDollEnvelopes {
+    Comparator<int[]> c = (o1 , o2) -> {
+        if(o1[0] != o2[0]){
+            return o1[0] - o2[0];
+        }else{
+            return o2[1] - o1[1];
+        }
+    };
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+
+        int n = envelopes.length;
+        Arrays.sort(envelopes, c);
+
+        List<Integer> f = new ArrayList<>();
+        f.add(envelopes[0][1]);
+        for (int i = 1; i < n; ++i) {
+            int num = envelopes[i][1];
+            if (num > f.get(f.size() - 1)) {
+                f.add(num);
+            } else {
+                int index = binarySearch(f, num);
+                f.set(index, num);
+            }
+        }
+        return f.size();
+    }
+
+    public int binarySearch(List<Integer> f, int target) {
+        int low = 0, high = f.size() - 1;
+        while (low + 1 < high) {
+            int mid = (high - low) / 2 + low;
+            if (f.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+
+}
