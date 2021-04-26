3088059de1b3a4ba6fbe32377c4629ac2fc562b7
liyanjie2 <liyanjie2@yy.com>
2021-04-26 10:44:15 +0800

    机器人运动范围

---
+++ src/main/java/study/bd/jzoffer/MovingCount.java
@@ -0,0 +1,44 @@
+package study.bd.jzoffer;
+
+/**
+ * 剑指 Offer 13. 机器人的运动范围
+ * 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：m = 2, n = 3, k = 1
+ * 输出：3
+ * 示例 2：
+ *
+ * 输入：m = 3, n = 1, k = 0
+ * 输出：1
+ * 提示：
+ *
+ * 1 <= n,m <= 100
+ * 0 <= k <= 20
+ *
+ *数位和增量公式： 设 x 的数位和为 sx, x+1 的数位和为 s(x+1)
+ * 1. 当 (x+1) % 10 = 0, s(x+1) = sx - 8, 如 19,20 的数位和分别为 10, 2
+ * 1. 当 (x+1) % 10 != 0, s(x+1) = sx + 1, 如 1,2 的数位和分别为 1, 2
+ *
+ * @author liyanjie
+ * @createTime 2021-04-26 10:27
+ */
+public class MovingCount {
+
+    int m, n, k;
+    boolean[][] visited;
+    public int movingCount(int m, int n, int k) {
+        this.m = m; this.n = n; this.k = k;
+        this.visited = new boolean[m][n];
+        return dfs(0, 0, 0, 0);
+    }
+    public int dfs(int i, int j, int si, int sj) {
+        if(i >= m || j >= n || k < si + sj || visited[i][j]) return 0;
+        visited[i][j] = true;
+        return 1 + dfs(i + 1, j, (i + 1) % 10 != 0 ? si + 1 : si - 8, sj) + dfs(i, j + 1, si, (j + 1) % 10 != 0 ? sj + 1 : sj - 8);
+    }
+
+}
