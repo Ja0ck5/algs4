6c5e7fa5b6513a7d347e8b53104002ed3f47ab12
liyanjie2 <liyanjie2@yy.com>
2021-04-29 09:59:20 +0800

    TreeSubStructure

---
+++ src/main/java/study/bd/jzoffer/TreeSubStructure.java
@@ -0,0 +1,25 @@
+package study.bd.jzoffer;
+
+import study.binarytree.TreeNode;
+
+/**
+ * @author liyanjie
+ * @createTime 2021-04-29 09:58
+ */
+public class TreeSubStructure {
+
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    }
+
+    boolean recur(TreeNode A, TreeNode B) {
+        if (B == null) {
+            return true;
+        }
+        if (A == null || A.val != B.val) {
+            return false;
+        }
+        return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+
+}
