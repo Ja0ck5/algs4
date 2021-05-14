fb5007949aee30cfbb0e7dfe5cdd86ba6e224130
liyanjie2 <liyanjie2@yy.com>
2021-05-14 11:44:25 +0800

    FirstMissingPositive

---
+++ src/main/java/study/bd/FirstMissingPositive.java
@@ -0,0 +1,74 @@
+package study.bd;
+
+/**
+ *
+ * 41. 缺失的第一个正数
+ * 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+ *
+ *
+ *
+ * 进阶：你可以实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案吗？
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,0]
+ * 输出：3
+ * 示例 2：
+ *
+ * 输入：nums = [3,4,-1,1]
+ * 输出：2
+ * 示例 3：
+ *
+ * 输入：nums = [7,8,9,11,12]
+ * 输出：1
+ *
+ *
+ * 提示：
+ *
+ * 0 <= nums.length <= 300
+ * -231 <= nums[i] <= 231 - 1
+ *
+ * https://leetcode-cn.com/problems/first-missing-positive/
+ *
+ * @author liyanjie
+ * @createTime 2021-05-14 10:02
+ */
+public class FirstMissingPositive {
+
+    /**
+     * 由于我们只在意 [1, N][1,N] 中的数，因此我们可以先对数组进行遍历，把不在 [1, N][1,N] 范围内的数修改成任意一个大于 NN 的数（例如 N+1N+1）。这样一来，数组中的所有数就都是正数了，因此我们就可以将「标记」表示为「负号」。算法的流程如下：
+     *
+     * 我们将数组中所有小于等于 00 的数修改为 N+1N+1；
+     *
+     * 我们遍历数组中的每一个数 xx，它可能已经被打了标记，因此原本对应的数为 |x|∣x∣，其中 |\,|∣∣ 为绝对值符号。如果 |x| \in [1, N]∣x∣∈[1,N]，那么我们给数组中的第 |x| - 1∣x∣−1 个位置的数添加一个负号。注意如果它已经有负号，不需要重复添加；
+     *
+     * 在遍历完成之后，如果数组中的每一个数都是负数，那么答案是 N+1N+1，否则答案是第一个正数的位置加 11。
+     *
+     * @param nums
+     * @return
+     */
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+
+}
